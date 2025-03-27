47b130ed86439b0c08b9e6671df29fe6d95aa00a
Shubhansh Mishra <shubhansh77@gmail.com>
2025-03-27 13:07:54 +0530

    Update NewsViewModel.kt

--- app/src/main/java/com/example/assignnews/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/assignnews/viewmodel/NewsViewModel.kt
@@ -13,7 +13,7 @@ import com.example.assignnews.model.Article
 import kotlinx.coroutines.flow.Flow
 
 class NewsViewModel:ViewModel() {
-    private val apiKey = "35f43e2f984142a59f671d92adfceb24" // Replace with your NewsAPI key
+    private val apiKey = "YOUR_API_KEY // Replace with your NewsAPI key
     private val query = "technology"
 
     val news: Flow<PagingData<Article>> = Pager(
